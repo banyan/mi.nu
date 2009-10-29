80d4ac61f377e4e7bf15ba0941c1e82976b7832f
banyan <ameutau@gmail.com>
2009-10-30 00:08:40 +0900

    when background is white, h1 is yellow

--- public/js/application.js
+++ public/js/application.js
@@ -2,6 +2,13 @@ var Minu = function() {};
 
 Minu.MAX_H1_FONT_SIZE = 2000;
 
+/**
+ * Constructer
+ */
+function Minu() {
+	this.is_background_white;
+}
+
 Minu.prototype = {
 
 	init: function() {
@@ -15,15 +22,19 @@ Minu.prototype = {
 			"#f0f0f0", // grey
 			"#00cccc", // dark_blue
 			"#3399ff", // blue
-			"#dd42ab"  // pink
+			"#ed0c8c"  // pink
 		];
 		var random_key = (Math.floor( Math.random() * colors.length));
+		this.is_background_white = (random_key === 0);
 		$("body").css("background-color", colors[random_key]);
 	},
 
 	_setH1FontSize: function() {
 		var random_size = (Math.floor( Math.random() * Minu.MAX_H1_FONT_SIZE));
 		$('h1').css("font-size", random_size);
+		if (this.is_background_white) {
+			$('h1').css("color", "#ffff00");
+		}
 	}
 
 }
